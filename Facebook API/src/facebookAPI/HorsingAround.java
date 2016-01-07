352f761408b210897391f7e0faa79f433fc86a63
alexjrosh <alexjr328@gmail.com>
2016-01-06 20:10:49 -0500

    Touchdown

--- Facebook API/src/facebookAPI/HorsingAround.java
+++ Facebook API/src/facebookAPI/HorsingAround.java
@@ -9,14 +9,11 @@ import com.mashape.unirest.request.GetRequest;
 
 public class HorsingAround {
 
-	public static final String ACCESS_TOKEN = "CAACEdEose0cBAPVndMOzVA7cj1uV82cZBPwelnXeMCbGa51KvZAGbUzNA0VJc7jxKnMyXkEZACOfg6yweKB09CSZChoZBXHkGJPyt0MyqR0VLW2bpvWloeIsGlsstYwdWNF8xGKi3YKWsBFiZAngiy6VnreM41QEuDziTyqDjPunGKZB3RaNnxOKuMVlhNsfuwlaFaUkuQnAZC7sZAxctI8nE";
+	public static final String ACCESS_TOKEN = "CAACEdEose0cBAP9PSOcoZCoyXDo80a8N7g1x2ZBJIRmK5KN2psxGEFMjn4zBXEGTbvTQUZCSqZAgh6swIj5XQ74DFSQcXBu5qoIN4J2umGi97sRFVaRk5XtiOcyGicKcb1BVG7hHLT9ISEtyR70uKBOjMsd0HtahanMlOFiQQ6Jft9h5B1ZAZBKNrt8t9U2beLWgZChpeQRwfkzDcPI7ToB";
 
 	public static void main(String[] args) {
 		try {
-			HttpResponse<JsonNode> get = Unirest.get("http://graph.facebook.com/v2.5/me")
-			.queryString("access_token", ACCESS_TOKEN)
-		    .queryString("fields", "name")
-			.asJson();
+			HttpResponse<JsonNode> get = Unirest.get("https://graph.facebook.com/v2.5/me?access_token="+ACCESS_TOKEN+"&fields=name").asJson();
 			System.out.println(get.getBody());
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
