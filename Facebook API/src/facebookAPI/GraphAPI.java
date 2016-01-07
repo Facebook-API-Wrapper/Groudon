4ec71f7f2fcf0979094a2f9880de1fa4faa7e0ff
alexjrosh <alexjr328@gmail.com>
2016-01-07 12:24:23 -0500

    Now we have some API

--- Facebook API/src/facebookAPI/GraphAPI.java
+++ Facebook API/src/facebookAPI/GraphAPI.java
@@ -1,5 +1,7 @@
 package facebookAPI;
 
+import java.util.Date;
+
 import org.json.JSONObject;
 
 import com.mashape.unirest.http.HttpResponse;
@@ -27,14 +29,32 @@ public class GraphAPI {
 				.getBody();
 	}
 	
-	public JSONObject getName(String id) throws UnirestException{
-		return this.getField(id, NAME_PARAM).getObject();
+	public String getName(String id) throws UnirestException{
+		JSONObject info = (JSONObject) this.getField(id, NAME_PARAM).getObject().get(id);
+		return info.getString(NAME_PARAM);
 	}
 	
-	public JsonNode getBirthday(String id) throws UnirestException{
-		return this.getField(id, BIRTHDAY_PARAM);
+	public Date getBirthday(String id) throws UnirestException{
+		JSONObject info = (JSONObject) this.getField(id, BIRTHDAY_PARAM)
+				.getObject()
+				.get(id);
+		String date = info.getString(BIRTHDAY_PARAM);
+		int month = Integer.parseInt(date.substring(0, 2));
+		int day = Integer.parseInt(date.substring(3,5));
+		int year = Integer.parseInt(date.substring(6,10));
+		Date birthday = new Date(year-1900, month-1, day);
+		return birthday;
 	}
 	
-	
+	public static void main(String[] args) throws UnirestException{
+		try{
+			GraphAPI test = new GraphAPI("CAACEdEose0cBAK8gitk1zchOnDZCvCeZB6JEgIAnyREZBismAK11cusqoLxmmZAeLZAKZALABQoZCCQZAmfYDODLCQxdjwrXTlQ1apui2R8B707SoLKsrme9xOEVIkXO3oyJNQ7MqHsxhzrZATbLEZCsuZBcRdivlgyuZCD44ZAoJGX3k3eOKFVX2lzlZAyrlSqbuVe4GlxFAU022OirEQYpKZC8Lvl");
+			System.out.println(test.getName("613415685427880"));
+			System.out.println(test.getBirthday("613415685427880"));
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 	
 }
